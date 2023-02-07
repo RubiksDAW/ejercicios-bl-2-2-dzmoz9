9e48cc8136c23a9dd13abd9264e75508798909a9
RubiksDAW <92635610+RubiksDAW@users.noreply.github.com>
2023-02-07 19:54:59 +0100

    1 file modified

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,10 +1,25 @@
-'Use strict';
+// Ejercicio 4:
+
+// Crea una copia y ordena este array por Nombre seguido de Edad y por último Apellido.
 
 let personas = [
-  { nombre: 'Juanma', apellido: 'Cano', edad: 22 },
-  { nombre: 'Juanma', apellido: 'Armario', edad: 45 },
-  { nombre: 'David', apellido: 'Quiñones', edad: 18 },
-  { nombre: 'Alberto', apellido: 'Rus', edad: 18 },
-  { nombre: 'Alberto', apellido: 'Rus', edad: 24 },
-  { nombre: 'Alberto', apellido: 'Rodriguez', edad: 80 },
+  { nombre: "Juanma", apellido: "Cano", edad: 22 },
+  { nombre: "Juanma", apellido: "Armario", edad: 45 },
+  { nombre: "David", apellido: "Quiñones", edad: 18 },
+  { nombre: "Alberto", apellido: "Rus", edad: 18 },
+  { nombre: "Alberto", apellido: "Rus", edad: 24 },
+  { nombre: "Alberto", apellido: "Rodriguez", edad: 80 },
 ];
+
+// Hacemos uso del metodo .slice() para devolver una copia del array original y .sort() para ordenar este mismo.
+let personasOrdenadas = personas.slice().sort((a, b) => {
+    if (a.nombre < b.nombre) return -1;
+    if (a.nombre > b.nombre) return 1;
+    if (a.edad < b.edad) return -1;
+    if (a.edad > b.edad) return 1;
+    if (a.apellido < b.apellido) return -1;
+    if (a.apellido > b.apellido) return 1;
+    return 0;
+  });
+
+console.log(personasOrdenadas)
